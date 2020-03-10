b5fdb8cb47c351d929ef829b85984d0c2c7529b0
otajisan <taji.taji.otajisan@gmail.com>
2020-03-11 08:06:15 +0900

    :+1: add 404 page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
 import { BlogComponent } from './blog/blog.component';
+import { PageNotFoundComponent } from './error/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IndexComponent,
-    BlogComponent
+    BlogComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
