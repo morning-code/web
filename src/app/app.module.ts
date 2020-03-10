2923d441c97540a2575dfa2a5c6256d0055e882a
otajisan <taji.taji.otajisan@gmail.com>
2020-03-11 08:04:47 +0900

    :+1: add index and blog components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
